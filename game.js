3c0b223c04792d00bdce86257fbbc815b04fdaa2
TerraGeode <77182889+TerraGeode@users.noreply.github.com>
2024-08-27 13:46:07 -0400

    Added playerChoose function, choices to hold options

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log("Hello, World");
+choices = ["Rock", "Paper", "Scissors"]
+//get human choice
+let playerChoose = () => {
+    choice = parseInt(prompt("Rock (1), Paper(2), Scissors(3)!"));
+    return choice;
+}
+//get computer choice
+//compare choices
+//track winner
+
+hChoice = playerChoose();
+console.log(choices[hChoice-1]);
